6f4bde2962e86692d41409094268bebbd0cfd911
Efim Nushtaev <efnushtaev@gmail.ru>
2026-07-27 13:33:38 +0300

    fix: remove publishRelayState (ESP32 self-loop caused relay toggling)

--- saspik-iot/ag-iot-sketches/esp32-controller/src/mqtt_handler.h
+++ saspik-iot/ag-iot-sketches/esp32-controller/src/mqtt_handler.h
@@ -5,7 +5,6 @@
 
 // Объявление функций
 void controlRelay(int relayPin, const char* relayName, const char* state);
-void publishRelayState(const char* topic, const char* state);
 void mqttCallback(char *topic, uint8_t *payload, unsigned int length);
 
 #endif
